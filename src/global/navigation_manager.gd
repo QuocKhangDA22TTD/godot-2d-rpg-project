fe22d757de41479487643bf726d1a3d3d840740c
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2025-11-27 15:19:55 +0700

    Thêm vào các tile mới của map hầm ngục và map indoor, thêm các door để di chuyển sang map indoor, thêm file báo cáo và file đề cương chi tiết.

--- src/global/navigation_manager.gd
+++ src/global/navigation_manager.gd
@@ -3,6 +3,7 @@ extends Node
 var scene_main_menu = preload("res://scenes/ui/main_menu.tscn")
 var scene_main_map = preload("res://scenes/map/main_map.tscn")
 var scene_level_1 = preload("res://scenes/map/level_1.tscn")
+var in_door = preload("res://scenes/map/indoor.tscn")
 
 signal on_trigger_player_spawn
 
@@ -16,6 +17,8 @@ func go_to_level(level_tag, destination_tag):
 			scene_to_load = scene_main_map
 		"level_1":
 			scene_to_load = scene_level_1
+		"in_door":
+			scene_to_load = in_door
 	
 	if scene_to_load != null:
 		TransitionScreen.transition()
